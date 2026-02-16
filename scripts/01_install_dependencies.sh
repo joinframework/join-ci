17a64046e4d86575f47b293eda6da9a5af47c92c
Mathieu Rabine <rabine.mathieu@gmail.com>
2026-02-16 22:15:12 +0100

    install libnuma (#6)
    Co-authored-by: mrabine <mathieu.rabine@vector.com>

--- scripts/01_install_dependencies.sh
+++ scripts/01_install_dependencies.sh
@@ -11,7 +11,10 @@ apt-get -qq install \
   unzip \
   git \
   cmake \
+  cmake-data \
+  pkg-config \
   build-essential \
+  ninja-build \
   wget \
   python3 \
   python3-pip \
@@ -24,6 +27,7 @@ apt-get -qq install \
   libjson-xs-perl \
   libcurl4-openssl-dev \
   libssl-dev \
+  libnuma-dev \
   zlib1g-dev \
   libicu-dev \
   libxml2-dev \
