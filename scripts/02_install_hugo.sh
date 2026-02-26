49f20b46e39e7deab431de093bfe0a220e60e5c7
Mathieu Rabine <rabine.mathieu@gmail.com>
2026-02-27 00:11:27 +0100

    arch support for hugo

--- scripts/02_install_hugo.sh
+++ scripts/02_install_hugo.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 HUGO_VERSION="0.152.2"
+ARCH=$(uname -m)
 
-wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
-tar -xvzf hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+if [ "$ARCH" = "x86_64" ]; then
+    HUGO_ARCH="amd64"
+elif [ "$ARCH" = "aarch64" ]; then
+    HUGO_ARCH="arm64"
+else
+    echo "arch not supported : $ARCH"
+    exit 1
+fi
+
+wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-${HUGO_ARCH}.tar.gz
+tar -xvzf hugo_extended_${HUGO_VERSION}_linux-${HUGO_ARCH}.tar.gz
 mv hugo /usr/local/bin/
-rm hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz LICENSE README.md
+rm hugo_extended_${HUGO_VERSION}_linux-${HUGO_ARCH}.tar.gz LICENSE README.md
